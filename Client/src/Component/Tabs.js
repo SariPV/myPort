78f7e23b47fe5e4f58c3a99f5f32f6f50fa34a2f
paranan Vitpornnitipacha <paranan.sarika@gmail.com>
2022-11-16 22:11:10 +0700

    update tabs

--- Client/src/Component/Tabs.js
+++ Client/src/Component/Tabs.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
@@ -9,19 +9,25 @@ import "./Tabs.css"
 
 const ProfileTabs = () => {
 
-    const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState('1');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-    return (
-        <>
-        <div className='tabs-box'>
-        
+  const [file, setFile] = useState();
+  function handleChangefile(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
 
-      
-      <Box sx={{ width: '50%', typography: 'body1' }}>
+  return (
+    <>
+      <div className='tabs-box'>
+
+
+
+        <Box sx={{ width: '50%', typography: 'body1' }}>
           <TabContext value={value}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
               <TabList onChange={handleChange} aria-label="lab API tabs example">
@@ -30,15 +36,61 @@ const ProfileTabs = () => {
                 <Tab label="Experience" value="3" />
                 <Tab label="Achievement" value="4" />
                 <Tab label="Skill" value="5" />
-                <Tab label="References" value="5" />
+                <Tab label="References" value="6" />
 
               </TabList>
             </Box>
-            <TabPanel value="1">Item One</TabPanel>
-            <TabPanel value="2">Item Two</TabPanel>
-            <TabPanel value="3">Item Three</TabPanel>
-            <TabPanel value="4">Item Four</TabPanel>
-            <TabPanel value="5">Item Five</TabPanel>
+            <TabPanel value="1">Profile Image
+
+
+              <form className="box" encType="multipart/form">
+                <div className="field mt-5">
+                  <input type="file" onChange={handleChangefile} /><br></br>
+                  <img className="profileImg" src={file} />
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Email</label>
+                  <div className="controls">
+                    <input type="text" className="input" placeholder="Email" />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Name</label>
+                  <div className="controls">
+                    <input type="text" className="input" placeholder="Name" />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Address</label>
+                  <div className="controls">
+                    <input type="text" className="input" placeholder="Address" />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Phone</label>
+                  <div className="controls">
+                    <input type="number" className="input" placeholder="Phone" />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Position</label>
+                  <div className="controls">
+                    <input type="text" className="input" placeholder="Position" />
+                  </div>
+                </div>
+
+                <div className="field mt-5">
+                  <button className="Loginbutton">Save</button>
+                </div>
+              </form>
+
+
+            </TabPanel>
+            <TabPanel value="2">Education</TabPanel>
+            <TabPanel value="3">Experience</TabPanel>
+            <TabPanel value="4">Achievement</TabPanel>
+            <TabPanel value="5">Skill</TabPanel>
+            <TabPanel value="6">References</TabPanel>
           </TabContext>
         </Box></div></>
   )
